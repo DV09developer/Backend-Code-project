b098175ef75ed565d059fea0769563321f350056
DV09developer <divyeshvoriya3@gmail.com>
2026-01-03 18:27:38 +0530

    utils code of error , response and asyncHandler.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 // require("dotenv").config({path : "./.env"})
 import dotenv from "dotenv";
 import connect_db from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({ 
     path: "./.env" 
 });
-connect_db();
+
+connect_db()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+})
 
 
 
